ac687a15c2144ddbfe26f751c78e379c8432a16d
Dercio Nacare <dpvnacare@gmail.com>
2020-10-17 18:48:34 +0200

    Mapeando as entidades Restaurante e Cozinha com JPA

---
+++ src/main/java/com/dn/code/food/domain/model/Restaurante.java
@@ -0,0 +1,60 @@
+package com.dn.code.food.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurante 
+{
+	@Id
+	private Long codigo;
+	
+	@Column(name = "taxa_frete")
+	private BigDecimal taxaFrete;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurante other = (Restaurante) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public BigDecimal getTaxaFrete() {
+		return taxaFrete;
+	}
+
+	public void setTaxaFrete(BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
+	}
+	
+	
+}
