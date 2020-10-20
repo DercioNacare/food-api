0680640b442951903e922fd25025ee9d9405a1b3
Dercio Nacare <dpvnacare@gmail.com>
2020-10-20 12:27:57 +0200

    Implementação do GET Singleton Resource da entidade estado e cozinha

--- src/main/java/com/dn/code/food/api/controller/EstadoController.java
+++ src/main/java/com/dn/code/food/api/controller/EstadoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class EstadoController
 	public List<Estado> listar()
 	{
 		return estadoRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public Estado buscar(@PathVariable("codigo")Long codigo)
+	{
+		return estadoRepository.findById(codigo).get();
 	}
 }
