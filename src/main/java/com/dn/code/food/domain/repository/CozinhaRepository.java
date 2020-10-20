947a23e0816356f503fa8000797c3ec5070d90c1
Dercio Nacare <dpvnacare@gmail.com>
2020-10-20 11:49:34 +0200

    Implementação do GET Collection Resource cozinha

--- src/main/java/com/dn/code/food/domain/repository/CozinhaRepository.java
+++ src/main/java/com/dn/code/food/domain/repository/CozinhaRepository.java
@@ -1,6 +1,12 @@
 package com.dn.code.food.domain.repository;
 
-public interface CozinhaRepository 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dn.code.food.domain.model.Cozinha;
+
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> 
 {
 
 }
