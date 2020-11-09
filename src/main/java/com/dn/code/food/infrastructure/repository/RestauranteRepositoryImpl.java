0b74e7e79b87ec16502aa2e427f13be025e62adb
Dercio Nacare <dpvnacare@gmail.com>
2020-11-09 23:04:10 +0200

    Refatoração da consulta dinâmica de restaurantes para o uso d Criteria API

--- src/main/java/com/dn/code/food/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/dn/code/food/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,15 @@
 package com.dn.code.food.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.dn.code.food.domain.model.Restaurante;
 
@@ -22,39 +22,14 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal)
 	{
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 		
-		var parametros = new HashMap<String, Object>();
 		
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
+		criteria.from(Restaurante.class);
 		
-		if(StringUtils.hasLength(nome))
-		{
-			jpql.append("and nome like :nome ");
-			
-			parametros.put("nome", "%" + nome + "%"); 
-		}
-		
-		if(taxaFreteInicial != null)
-		{
-			jpql.append("and taxaFrete >= :taxaFreteInicial ");
-			
-			parametros.put("taxaFreteInicial", taxaFreteInicial);
-		}
-		
-		if(taxaFreteFinal != null)
-		{
-			jpql.append("and taxaFrete <= :taxaFreteFinal");
-			
-			parametros.put("taxaFreteFinal", taxaFreteFinal);
-		}
-		
-		TypedQuery<Restaurante> query = entityManager.createQuery(jpql.toString(), Restaurante.class);
+		TypedQuery<Restaurante> query = entityManager.createQuery(criteria);
 				
-		parametros.forEach((chave, valor) ->{
-			query.setParameter(chave, valor);
-		});
-		
 		return query.getResultList();
 	}
 }
