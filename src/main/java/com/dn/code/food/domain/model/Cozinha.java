b4c3b25a71586a7b954d98cb7e2479214e27b85d
Dercio Nacare <dpvnacare@gmail.com>
2020-11-13 12:25:19 +0200

    Mapeando relacionamento biderecional entre restaurante e cozinha com anotação OneToMany

--- src/main/java/com/dn/code/food/domain/model/Cozinha.java
+++ src/main/java/com/dn/code/food/domain/model/Cozinha.java
@@ -1,10 +1,15 @@
 package com.dn.code.food.domain.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -21,6 +26,10 @@ public class Cozinha
 	@EqualsAndHashCode.Include
 	private Long codigo;
 	
-	@Column(nullable = false	)
+	@Column(nullable = false)
 	private String nome;	
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes;
 }
